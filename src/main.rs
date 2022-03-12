666cfac53e4d8c95d68af195019822826b777609
ujjwal <ujjwal.kumar@mail.mcgill.ca>
2022-03-12 15:49:50 -0500

    removed comment

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
-// we are compiling for bare metal system :  thumbv7em-none-eabihf
-// because we don't need to use host operating system ( or library for our own operating system )
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
-#[no_mangle] // don't mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point , since the linkeer looks for a function
-    // named '_start' by default
     loop {}
 }
 
