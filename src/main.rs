d1e3c480e0a55e9f0f093cfd2c876ce863fa7c93
ujjwal <ujjwal.kumar@mail.mcgill.ca>
2022-03-13 14:52:48 -0400

    added static vga_buffer implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+#![allow(dead_code)]
 #![no_std]
 #![no_main]
+
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
@@ -11,12 +14,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; // for printing to console
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
+    write!(
+        vga_buffer::WRITER.lock(),
+        ", some numbers: {} {}",
+        42,
+        1.337
+    )
+    .unwrap();
     loop {}
 }
